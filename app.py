926465062354b368c9a77900bc197dde39f6c902
megane18 <meganealexis12@gmail.com>
2025-02-18 23:09:50 -0500

    Fix the requirements.txt

--- app.py
+++ app.py
@@ -117,16 +117,13 @@ load_dotenv()
 
 
 # Access variables
-try:
-    tmdb_access_token = st.secrets["general"]["TMDB_ACCESS_TOKEN"]
-    neo4j_uri = st.secrets["general"]["NEO4J_URI2"]
-    neo4j_user = st.secrets["general"]["NEO4J_USER2"]
-    neo4j_password = st.secrets["general"]["NEO4J_PASSWORD2"]
-    email_user = st.secrets["general"]["EMAIL_USER"]
-    email_password = st.secrets["general"]["EMAIL_PASSWORD"]
-    email_receiver = st.secrets["general"]["EMAIL_RECEIVER"]
-except KeyError as e:
-    st.error(f"Missing key in secrets: {e}")
+tmdb_access_token = st.secrets["general"]["TMDB_ACCESS_TOKEN"]
+neo4j_uri = st.secrets["general"]["NEO4J_URI2"]
+neo4j_user = st.secrets["general"]["NEO4J_USER2"]
+neo4j_password = st.secrets["general"]["NEO4J_PASSWORD2"]
+email_user = st.secrets["general"]["EMAIL_USER"]
+email_password = st.secrets["general"]["EMAIL_PASSWORD"]
+email_receiver = st.secrets["general"]["EMAIL_RECEIVER"]
 
 # neo4j_uri = os.getenv('NEO4J_URI2')
 # neo4j_user = os.getenv('NEO4J_USER2')
